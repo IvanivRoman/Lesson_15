78261f818e1b43f201a8d537d0fa10a196bcf898
Roman <ivaniv.roma.15@gmail.com>
2022-12-03 00:45:34 +0200

    implementing some functionality with functional interfaces

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,88 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Calendar;
+import java.util.function.*;
+
+public class App {
+    static UnaryOperator<Integer> fib;
+
+    public static void printRandomNumber(int n) {
+        IntConsumer random = number -> System.out.println(Math.round(Math.random() * number));
+        random.accept(n);
+    }
+
+    public static int dayOfWeek() {
+        IntSupplier dayOfWeek = () -> Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        return dayOfWeek.getAsInt();
+    }
+
+    public static long convertDoubleToLong(double number) {
+        DoubleToLongFunction convert = Math::round;
+        return convert.applyAsLong(number);
+    }
+
+    public static int getFibonacciNumber(int index) {
+        fib = n -> {
+            if (n == 0)
+                return 0;
+            else
+                return n == 1 || n == 2 ? 1 : fib.apply(n - 1) + fib.apply(n - 2);
+        };
+        return fib.apply(index);
+    }
+
+    public static boolean isPrime(int n) {
+        IntPredicate prime = number -> {
+
+            if(n == 0 || n == 1)
+                return false;
+
+            if (number % 2 == 0) {
+                return number == 2;
+            }
+
+            int limit = (int)(0.1 + Math.sqrt(number));
+            for (int i = 3; i <= limit; i += 2) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        return prime.test(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Random number from 0 to 10");
+        for (int i = 0; i < 3; i++) {
+            printRandomNumber(10);
+        }
+
+        System.out.println("********************************************************");
+
+        System.out.println("Number of this day is: " + dayOfWeek());
+
+        System.out.println("********************************************************");
+
+        System.out.println("Convert double to long");
+        System.out.println("4.2 -> " + convertDoubleToLong(4.2));
+        System.out.println("4.4 -> " + convertDoubleToLong(4.4));
+        System.out.println("4.5 -> " + convertDoubleToLong(4.5));
+
+        System.out.println("********************************************************");
+
+        System.out.println("Fibonacci number by index");
+        System.out.println("index: 1 -> " + getFibonacciNumber(1));
+        System.out.println("index: 7 -> " + getFibonacciNumber(7));
+        System.out.println("index: 13 -> " + getFibonacciNumber(13));
+
+        System.out.println("********************************************************");
+
+        System.out.println("Check is number prime");
+
+        for (int i = 0; i < 21; i++) {
+            System.out.println("is prime: " + i + " " + isPrime(i));
+        }
+
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,64 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import java.util.Calendar;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.assertj.core.api.Assertions.*;
+
+class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void testDayOfWeek() {
+        int todayDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        assertThat(todayDay).isEqualTo(App.dayOfWeek());
+    }
+
+    @Test
+    void testConvertDoubleToLong() {
+        long n1 = Math.round(4.0);
+        long n2 = Math.round(4.1);
+        long n3 = Math.round(4.2);
+        long n4 = Math.round(4.3);
+        long n5 = Math.round(4.4);
+        long n6 = Math.round(4.5);
+        long n7 = Math.round(4.6);
+        long n8 = Math.round(4.7);
+        long n9 = Math.round(4.8);
+        long n10 = Math.round(4.9);
+
+        assertThat(n1).isEqualTo(App.convertDoubleToLong(4.0));
+        assertThat(n2).isEqualTo(App.convertDoubleToLong(4.1));
+        assertThat(n3).isEqualTo(App.convertDoubleToLong(4.2));
+        assertThat(n4).isEqualTo(App.convertDoubleToLong(4.3));
+        assertThat(n5).isEqualTo(App.convertDoubleToLong(4.4));
+        assertThat(n6).isEqualTo(App.convertDoubleToLong(4.5));
+        assertThat(n7).isEqualTo(App.convertDoubleToLong(4.6));
+        assertThat(n8).isEqualTo(App.convertDoubleToLong(4.7));
+        assertThat(n9).isEqualTo(App.convertDoubleToLong(4.8));
+        assertThat(n10).isEqualTo(App.convertDoubleToLong(4.9));
+    }
+
+    @Test
+    void testGetFibonacciNumber() {
+        assertThat(App.getFibonacciNumber(0)).isZero();
+        assertThat(App.getFibonacciNumber(1)).isEqualTo(1);
+        assertThat(App.getFibonacciNumber(2)).isEqualTo(1);
+        assertThat(App.getFibonacciNumber(3)).isEqualTo(2);
+        assertThat(App.getFibonacciNumber(4)).isEqualTo(3);
+        assertThat(App.getFibonacciNumber(5)).isEqualTo(5);
+        assertThat(App.getFibonacciNumber(6)).isEqualTo(8);
+        assertThat(App.getFibonacciNumber(7)).isEqualTo(13);
+        assertThat(App.getFibonacciNumber(8)).isEqualTo(21);
+        assertThat(App.getFibonacciNumber(9)).isEqualTo(34);
+        assertThat(App.getFibonacciNumber(10)).isEqualTo(55);
+    }
+
+    @Test
+    void testPrimeNumbers() {
+        assertThat(App.isPrime(7)).isTrue();
+        assertThat(App.isPrime(9)).isFalse();
+        assertThat(App.isPrime(12)).isFalse();
+        assertThat(App.isPrime(17)).isTrue();
+        assertThat(App.isPrime(19)).isTrue();
     }
 }
